170b45747398beda0a8ab3eeaa07db04dd4e38dc
Tijana Radosavljevic <tijana.ux@gmail.com>
2022-12-15 16:49:53 +0100

    Drugi domaci cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+
+  "baseUrl": "https://gallery-app.vivifyideas.com/",
+
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
--- cypress/e2e/registre.cy.js
+++ cypress/e2e/registre.cy.js
@@ -1,4 +1,6 @@
- /* describe("Login tests", () =>{
+  
+  
+  /*describe("Login tests", () =>{
     it ('Visit gallery app', () => {
         cy.visit('https://gallery-app.vivifyideas.com/')
         cy.get('.nav-link').eq(1).click()
@@ -22,19 +24,19 @@
         // cy.get('.nav-link').eq(3).click()
     })
 })
-
 */
+
 
 
 describe("Register test", ()=>{
 
     it('Register tab', () => {
-    cy.visit('https://gallery-app.vivifyideas.com/')
+    cy.visit('/')
     cy.get('.nav-link').eq(2).click()
     });
 
     it ("Register valid credentials",() =>{
-    cy.visit('https://gallery-app.vivifyideas.com/')
+    cy.visit('/')
     cy.get('.nav-link').eq(2).click()
     cy.get('#first-name').type("tijana")
     cy.get('#last-name').type("radosavljevic")
